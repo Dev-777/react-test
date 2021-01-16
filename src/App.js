fd30fb3b50329c906f3318b1e5211707b9e20622
Dev-777 <arsen172317@gmail.com>
2021-01-16 23:58:36 +0400

    dd

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
+import { useRef } from "react";
+import axios from "axios";
 
 const App = () => {
+  const inputRef = useRef();
   const state = useSelector((state) => state);
-  const dispatch = useDispatch();
   const click = () => {
-    dispatch({ type: "CHANGE_NAME" });
+    axios
+      .post("https://sign-up-backend.herokuapp.com/app/test44", {
+        input: inputRef,
+      })
+      .then(() => console.log("post test179 dont"));
   };
+  const get = () => {
+    axios
+      .get("https://sign-up-backend.herokuapp.com/app/test33")
+      .then((data) => console.log(data, "data getting"));
+  };
+
   return (
     <div className="App">
       <h1>app {state.name}</h1>
-      <button onClick={click}>click</button>
+      <input ref={inputRef} type="text" />
+      <button onClick={click}>post</button>
+      <button onClick={get}>getData</button>
     </div>
   );
 };
